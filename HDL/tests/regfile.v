6cefb9ea0edb90f83fa29bd7ac942349db9f22da
Eris <hello@maculos.dev>
2024-05-04 13:54:32 -0500

    Initial commit

---
+++ HDL/tests/regfile.v
@@ -0,0 +1,20 @@
+/*
+RV64I CPU Project
+Module is based on version 20191213 of the Unprivileged RISC-V ISA Chapter(s) 
+XLEN = 64;
+*/
+
+`ifndef regfile
+`define regfile
+
+module regfile (
+    input clock,
+);
+
+
+
+
+
+endmodule
+
+`endif
---
+++ README.md
@@ -0,0 +1,2 @@
+# BOG-1
+ My attempt at creating a RISC-V cpu. Inital target is a fpga implementation of RV64I
